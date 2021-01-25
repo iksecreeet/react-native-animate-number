b0fd08f677cdcf132c7980a06a055e97013d946d
iksecreeet <iksecreeet@gmail.com>
2021-01-25 15:07:30 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -24,9 +24,7 @@ export default class AnimateNumber extends Component {
     timing : 'linear' | 'easeOut' | 'easeIn' | () => number,
     formatter : () => {},
     onProgress : () => {},
-    onFinish : () => {},
-    startAt? : number,
-    initialValue? : number
+    onFinish : () => {}
   };
 
   static defaultProps = {
@@ -34,7 +32,6 @@ export default class AnimateNumber extends Component {
     timing : 'linear',
     steps : 45,
     value : 0,
-    initialValue : 0,
     formatter : (val) => val,
     onFinish : () => {}
   };
@@ -80,8 +77,8 @@ export default class AnimateNumber extends Component {
     super(props);
     // default values of state and non-state variables
     this.state = {
-      value : props.initialValue,
-      displayValue : props.formatter(props.initialValue)
+      value : 0,
+      displayValue : 0
     }
     this.dirty = false;
     this.startFrom = 0;
@@ -92,14 +89,10 @@ export default class AnimateNumber extends Component {
     this.startFrom = this.state.value
     this.endWith = this.props.value
     this.dirty = true
-    setTimeout(
-      () => {
-        this.startAnimate()
-      }
-      , this.props.startAt != null ? this.props.startAt : 0);
+    this.startAnimate()
   }
 
-  componentWillUpdate(nextProps, nextState) {
+  UNSAFE_componentWillUpdate(nextProps, nextState) {
 
     // check if start an animation
     if(this.props.value !== nextProps.value) {
@@ -140,9 +133,8 @@ export default class AnimateNumber extends Component {
     Timer.setTimeout(() => {
 
       let value = (this.endWith - this.startFrom)/this.props.steps
-      let sign = value >= 0 ? 1 : -1
       if(this.props.countBy)
-        value = sign*Math.abs(this.props.countBy)
+        value = Math.sign(value)*Math.abs(this.props.countBy)
       let total = parseFloat(this.state.value) + parseFloat(value)
 
       this.direction = (value > 0)
